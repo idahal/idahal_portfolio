7386a73717231f013b7a5f089909e442e7593df7
Ida Hallner <bee_ida@hotmail.com>
2019-05-24 10:51:42 +0200

    import created components

--- portfolio/src/components/App/index.js
+++ portfolio/src/components/App/index.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router } from "@reach/router"
 import './App.css';
+import Navbar from "../Navbar";
+import Header from "../Header";
+import Projects from "../Projects";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Header />
+      <Router>
+        <Projects path="/projects"/>
+      </Router>
     </div>
   );
 }
