543ea4f9d6f175d450aab0f48ef1b2e612b2b0cb
Ida Hallner <bee_ida@hotmail.com>
2019-05-24 18:32:57 +0200

    add new components to app

--- portfolio/src/components/App/index.js
+++ portfolio/src/components/App/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Router } from "@reach/router"
+import { Router } from "@reach/router";
 import './App.css';
 import Navbar from "../Navbar";
 import Header from "../Header";
 import Projects from "../Projects";
+import projects from '../../data/projects';
+
+
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Header />
       <Router>
-        <Projects path="/projects"/>
+        <Header path="/"/>
+      </Router>
+
+      <Router>
+        <Projects items={projects} path="/projects"/>
       </Router>
     </div>
   );
