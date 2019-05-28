a42178c79b7ae5d632074134bf4b59455db6c72d
Ida Hallner <bee_ida@hotmail.com>
2019-05-28 20:24:01 +0200

    import logo i app

--- portfolio/src/components/App/index.js
+++ portfolio/src/components/App/index.js
@@ -3,6 +3,10 @@ import { Router } from "@reach/router";
 import './App.css';
 import Navbar from "../Navbar";
 import Header from "../Header";
+import HamburgerBar from "../HamburgerBar";
+import logo from './logo_big.svg';
+
+
 import Projects from "../Projects";
 import projects from '../../data/projects';
 
@@ -12,7 +16,9 @@ import projects from '../../data/projects';
 function App() {
   return (
     <div className="App">
+    <img src={logo} className="App-logo" alt="logo" />
       <Navbar />
+      <HamburgerBar />
       <Router>
         <Header path="/"/>
       </Router>
