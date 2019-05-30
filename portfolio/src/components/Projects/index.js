22e7e5911f4ba17dcc8ce791baaeebf82f54d59b
Ida Hallner <bee_ida@hotmail.com>
2019-05-30 15:41:18 +0200

    change structur on component

--- portfolio/src/components/Projects/index.js
+++ portfolio/src/components/Projects/index.js
@@ -1,31 +1,44 @@
-import React from "react";
-import styled from "styled-components";
-import ProjectsItem from '../ProjectsItem';
+import React, { Component } from 'react';
+import fastSecurityDesktop from '../../../src/images/fs_square.jpg';
+import memoryDesktop from '../../../src/images/memory_square.PNG';
+import photoifyDesktop from '../../../src/images/photoify_square.jpg';
+import ListOfProjects from "../ListOfProjects";
 
-const StyledProjects = styled.div`
-  @media screen and (max-width: 800px) {
 
+const projects = [  {
+      name: "Photoify",
+      image: photoifyDesktop,
+      description: "Ett skolprojekt där uppgiften var att skapa en Instragram-klon. Projektet är skrivet i HTML, CSS, Javascript och PHP. Projektet är uppbyggt genom en SQLite databas.",
+      link: "https://github.com/idahal/Photoify"
+  },
+  {
+      name: "Fuzzy-Memory",
+      image: memoryDesktop,
+      description: "Ett skolprojekt där uppgiften var att bygga ett memory-spel i HTML, CSS and Javascript.",
+      link: "https://github.com/idahal/Fuzzy-Memory",
+  },
+  {
+      name: "FastSecurity 1.0",
+      image: fastSecurityDesktop,
+      description: "Ett gemensamt skolprojekt av Yrgo-studenter från inriktningarna webbutveckling, digital design och CG design. Uppgiften var att skapa en landningsida för en ny produkt FastSecurity, FS1.0.",
+      link: "https://github.com/lovisahallgren/FastSecurity",
   }
-`;
+]
 
+class Projects extends Component {
+    render() {
+        return (
+          <div className="Projects">
+             {projects.map((e, key) => (
+                 <ListOfProjects
+                 key={key}
+                 image={e.image}
+                 description={e.description}
+                 />
+             ))}
+          </div>
 
-const Projects = props => {
-    return (
-      <StyledProjects>
-        <div className="Projects">
-           {props.items.map((item, key) => (
-               <ProjectsItem
-                    key={key}
-                    name={item.name}
-                    description={item.description}
-                    link={item.link}
-               />
-           ))}
-        </div>
-        </StyledProjects>
-    );
-};
-
-
+          )}
+}
 
 export default Projects;
