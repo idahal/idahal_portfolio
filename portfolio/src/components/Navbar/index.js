574cb83d9318377498728839f4b78c4fb7901c2a
Ida Hallner <bee_ida@hotmail.com>
2019-05-30 15:40:47 +0200

    add some styling

--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -5,23 +5,24 @@ import { Link } from "@reach/router";
 
 const StyledNavbar = styled.div`
     width: 100%;
-    heigth: 40vh;
+    height: 10vh;
     display: flex;
     flex-direction: row;
     justify-content: flex-end;
-    background-color: #1d191d;
+    background-color: #ffffff;
     font-size: 18px;
   a {
-    color: #ffffe5;
+    color: #1d191d;
     text-decoration: none;
     font-weight: 700;
     font-size: 18px;
     margin: 0 10px;
   }
   @media screen and (max-width: 800px) {
-
+    display: none;
   }
 `;
+
 
 const Navbar = () => {
         return (
