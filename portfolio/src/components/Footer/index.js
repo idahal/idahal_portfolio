21f2808633cf8a5ec3e6024c01c667c0bfcc2f6c
Ida Hallner <bee_ida@hotmail.com>
2019-06-02 20:14:54 +0200

    add new component

---
+++ portfolio/src/components/Footer/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from "styled-components";
+const StyledFooter = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #ffffff;
+  width: 100%;
+  height: 15vh;
+  color: #1d191d;
+
+hr {
+  text-decoration: none;
+  width: 80%;
+  border: 2px #1d191d solid;
+}
+
+p {
+  align-self: flex-end;
+}
+
+  @media screen and (max-width: 800px) {
+
+    hr {
+    }
+
+  }
+`;
+
+
+
+const Footer = () => (
+    <StyledFooter>
+        <hr></hr>
+        <p>Copyright Ida Hallner 2019</p>
+    </StyledFooter>
+)
+
+
+export default Footer;
