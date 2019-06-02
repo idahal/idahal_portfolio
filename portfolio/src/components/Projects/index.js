deec8ce72fcd5a0c8048a14b1351b992a2d35bdf
Ida Hallner <bee_ida@hotmail.com>
2019-06-02 20:15:21 +0200

    change soem styling

--- portfolio/src/components/Projects/index.js
+++ portfolio/src/components/Projects/index.js
@@ -1,30 +1,9 @@
 import React, { Component } from 'react';
 import styled from "styled-components";
-import fastSecurityDesktop from '../../../src/images/fs_square.jpg';
-import memoryDesktop from '../../../src/images/memory_square.PNG';
-import photoifyDesktop from '../../../src/images/photoify_square.jpg';
+
 import ListOfProjects from "../ListOfProjects";
+import projects from '../../../src/data/projects.js';;
 
-
-const projects = [  {
-      name: "Photoify",
-      image: photoifyDesktop,
-      description: "Ett skolprojekt där uppgiften var att skapa en Instragram-klon. Projektet är skrivet i HTML, CSS, Javascript och PHP. Projektet är uppbyggt genom en SQLite databas.",
-      link: "https://github.com/idahal/Photoify"
-  },
-  {
-      name: "Fuzzy-Memory",
-      image: memoryDesktop,
-      description: "Ett skolprojekt där uppgiften var att bygga ett memory-spel i HTML, CSS and Javascript.",
-      link: "https://github.com/idahal/Fuzzy-Memory",
-  },
-  {
-      name: "FastSecurity 1.0",
-      image: fastSecurityDesktop,
-      description: "Ett gemensamt skolprojekt av Yrgo-studenter från inriktningarna webbutveckling, digital design och CG design. Uppgiften var att skapa en landningsida för en ny produkt FastSecurity, FS1.0.",
-      link: "https://github.com/lovisahallgren/FastSecurity",
-  }
-]
 
 const StyledProjects = styled.div`
   display: flex;
@@ -43,7 +22,7 @@ class Projects extends Component {
   render() {
     return (
       <div className="Projects">
-       <StyledProjects>
+      <StyledProjects>
           {projects.map((e, key) => (
             <ListOfProjects
               key={key}
@@ -52,7 +31,7 @@ class Projects extends Component {
               link={e.link}
             />
          ))}
-      </StyledProjects>
+         </StyledProjects>
     </div>
   )}
 }
