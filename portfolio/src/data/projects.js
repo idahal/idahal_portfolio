e00f105e476118280730c94cc6ba2f4f24334050
Ida Hallner <bee_ida@hotmail.com>
2019-06-02 20:14:31 +0200

    add data in seperate file

---
+++ portfolio/src/data/projects.js
@@ -0,0 +1,25 @@
+import fastSecurityDesktop from '../../src/images/fs_square.jpg';
+import memoryDesktop from '../../src/images/memory_square.PNG';
+import photoifyDesktop from '../../src/images/photoify_square.jpg';
+
+const projects = [  {
+      name: "Photoify",
+      image: photoifyDesktop,
+      description: "This is a project written in HTML, CSS, Javascripts and PHP. The project är build throw a SQLIte database. This is a shcool project where the assignment was to create a Instragram clone.",
+      link: "https://github.com/idahal/Photoify"
+  },
+  {
+      name: "Fuzzy-Memory",
+      image: memoryDesktop,
+      description: "A memory game. This project is written in HTML, CSS and Javascript. Try to find matching card and play agian when you win.",
+      link: "https://github.com/idahal/Fuzzy-Memory",
+  },
+  {
+      name: "FastSecurity 1.0",
+      image: fastSecurityDesktop,
+      description: "A landing page. This is a school project by Yrgo students of Web Development, Digital Designer and Computer Graphics Design. The assignment was to create a landing page for a new product FastSecurity, FS1.0.",
+      link: "https://github.com/lovisahallgren/FastSecurity",
+  }
+]
+
+export default projects;
